d67e3d9d6a3948fe0e219047e64dffa2b67d526b
Carlos Rojas <Carlos.Y.Rojas.-ND@disney.com>
2018-11-18 13:18:10 -0500

    init

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent {
+
+  isOn = false;
+
+  get message() {
+    return `El test es ${this.isOn ? 'On' : 'Off'}`; 
+  }
+
+  clicked() {
+    this.isOn = !this.isOn;
+  }
+}
